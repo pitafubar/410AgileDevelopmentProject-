9527e2dc24fec3ef759d53659e49cfbb38e8e95e
Miller Ridgeway <Miller Ridgeway>
2015-11-02 18:57:08 -0700

    Updated the description on the One Word Parse Test, putting in a line for usage and a bit of an explanation.

--- AgileNLP/src/agilenlp/TestHarness/OneWordParseTest.java
+++ AgileNLP/src/agilenlp/TestHarness/OneWordParseTest.java
@@ -11,6 +11,15 @@ import java.util.Scanner;
 
 public class OneWordParseTest {
 
+/*
+    A one word parse test that takes a single word/phrase querey, and returns an Array List of MovieComposite
+    objects that contain that the querey. Currently untested, but leaves little room for error. 
+    
+    Usage: ArrayList<MovieComposite> foo = Class.parseOneWord()
+    
+    This creates a new array list with a parsed set of data, prompt is included in the method. May be more
+    practical to move the prompt later. 
+    */
     public static void main(String[] args) {
         ArrayList<MovieComposite> selectedMovies = parseOneWord();
         for(MovieComposite m: selectedMovies )
